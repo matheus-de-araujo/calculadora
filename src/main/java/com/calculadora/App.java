be66e8b37108c6556844be9dae522b44f9d781a3
Ismael-Vilacian <ismaelv161@gmail.com>
2022-05-11 22:30:47 -0300

    Adicionado tratamento para quando o tem entrada diferente de to tipo da variavel

--- src/main/java/com/calculadora/App.java
+++ src/main/java/com/calculadora/App.java
@@ -10,25 +10,38 @@ public class App {
 
     private static void InicializarInterfaceDeInteracao() {
         Scanner entrada = new Scanner(System.in);
+        int operacao = 0;
         
         while (true) {
+            
             System.out.println("Gama Tecnologia");
             System.out.println("Calculadora - Ver. 2.0");
 
             System.out.println("Selecione a operação:");
             System.out.println("1- Soma    2- Subtração    3- Multiplicação    4- Divisão    0- Finalizar");
-            int operacao = entrada.nextInt();
+            operacao = entrada.nextInt();
 
-            if (operacao == 0) break;
+            if (operacao == 0)
+                break;
 
             Double somatorio = 0.0;
             while (true) {
 
-                System.out.println("Digite a operação (para finalizar digite 'sair'):");
-                String entradaDoUsuario = entrada.nextLine();
+                Double entradaDoUsuario = 0.01;
+                System.out.println("Digite um valor (para finalizar digite 'sair'):");
 
-                if (entradaDoUsuario.toUpperCase().equals("SAIR")) {
-                    break;
+                try {
+                    entradaDoUsuario = entrada.nextDouble();
+                } catch (Exception e) {
+                    if (entradaDoUsuario.toString().toUpperCase().equals("SAIR")) {
+                        entradaDoUsuario = 0.0;
+                        break;
+                    }
+                    else {
+                        System.out.println("ATENÇÃO, valor invalido!");
+                        entradaDoUsuario = 0.0;
+                        break;
+                    }
                 }
             }
         }
