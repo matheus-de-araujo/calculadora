92d24d585f8d2b1ea72c152146e5b0d873204a89
Matheus de Araujo <matheus.encode@gmail.com>
2022-05-11 22:41:03 -0300

    CAL-13 - Adicionado a classe Calculadora básica.

--- src/main/java/com/calculadora/App.java
+++ src/main/java/com/calculadora/App.java
@@ -4,6 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "First pull request!" );
+        CalculadoraBasica cal = new CalculadoraBasica(2, 3);
+        cal.soma();
+        System.out.println(cal.getResultado());
     }
 }
