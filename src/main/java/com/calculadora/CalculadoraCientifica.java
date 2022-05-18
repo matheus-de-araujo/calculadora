80813c8f88b6fa7f31c3db923beb9b32a0cf701e
Matheus de Araujo <matheus.encode@gmail.com>
2022-05-18 20:48:02 -0300

    CAL-28 - Adicionado a operação de tangenciação.

--- src/main/java/com/calculadora/CalculadoraCientifica.java
+++ src/main/java/com/calculadora/CalculadoraCientifica.java
@@ -12,4 +12,8 @@ public class CalculadoraCientifica extends CalculadoraBasica{
     public void raizQuadrada() {
         super.setResultado(Math.sqrt(this.getTermoUm()));
     }
+
+    public void tangente() {
+        super.setResultado(Math.tan(this.getTermoUm()));
+    }
 }
